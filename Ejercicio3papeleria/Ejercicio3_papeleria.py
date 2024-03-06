de877612fa772b7ac391494107878aa788c24b36
juandiaz1301 <juandiaz@colegioguanenta.edu.co>
2024-03-06 07:13:13 -0500

    Se realizo analisis y diseño

--- Ejercicio3papeleria/Ejercicio3_papeleria.py
+++ Ejercicio3papeleria/Ejercicio3_papeleria.py
@@ -1,14 +1,21 @@
-def calcular_precio_venta(precio_costo):
-    if precio_costo < 3000:
-        ganancia = precio_costo * 0.15
-    elif 3000 <= precio_costo <= 6000:
-        ganancia = 500
-    else:
-        ganancia = precio_costo * 0.25
-    
-    precio_venta = precio_costo + ganancia
-    return precio_venta
+import math
+# Programa para calcular el porcentaje de ganancia de un articulo vendido
 
-precio_costo = float(input("Ingrese el precio de costo del artículo: "))
-precio_venta = calcular_precio_venta(precio_costo)
-print("El precio de venta del artículo es:", precio_venta)
+# input
+Precio_costo = float(input ("Ingrese el valor del producto "))
+Precio_costo = float(input ("Ingrese el valor del producto: "))
+Ganancia = 0
+Precio_venta = Precio_costo+Ganancia
+
+# processing 
+
+if Precio_costo < 3000:
+    Ganancia = Precio_costo *0.15
+elif  Precio_costo > 6000:
+    Ganancia = Precio_costo *0.25
+else:
+    Ganancia = 500
+
+Precio_venta = (Precio_costo + Ganancia)
+# output
+print("El producto tiene un precio final de",Precio_venta,)
